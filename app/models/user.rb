cb8576dee5cfac4d238a289e00c5944c82378a66
Greg0109 <greg.rabago@gmail.com>
2021-03-02 13:40:19 +0100

    Update README & fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length: { minimum: 5 }
-    validates :email, presence: true, length: { minimum: 10 }, format: { with: /[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+/, message: "Email invalid" }
-    validates :password, presence: true, length: { minimum: 8 }
+  validates :username, presence: true, length: { minimum: 5 }
+  validates :email, presence: true, length: { minimum: 10 },
+                    format: { with: /[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+/, message: 'Email invalid' }
+  validates :password, presence: true, length: { minimum: 8 }
 end
