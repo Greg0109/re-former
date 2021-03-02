e38a7ba09625170a49993ccb8760060ed2ef2ef1
juxsalley <juxsalley@gmail.com>
2021-03-02 11:20:21 +0000

    view and edit form structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update,]
   # get 'users', to: 'users#new'
-  post 'user', to: 'users#create'
+
+ 
 end
